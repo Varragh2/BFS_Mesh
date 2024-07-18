f419a3de837fad5d2c118d02184b21e6ea24af6f
Varrragh2 <nolant190@gmail.com>
2024-07-18 15:59:10 -0700

    Still adding Gradle

--- src/main/InstallEndUserEquipmentSquare.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class InstallEndUserEquipmentSquare extends Square {
-    @Override
-    public void run(Player player) {
-        System.out.println("Welcome to Install End User Equipment.\nThis square will give you the tools to connect to the mesh network.");
-    }
-}
