6f751090f9770755125f16b798ee8c80353c1137
Varrragh2 <nolant190@gmail.com>
2024-08-01 19:28:53 -0700

    Getting ready to submit project again.

---
+++ src/test/java/com/thomasnolan/EducateYourselfTest.java
@@ -0,0 +1,46 @@
+package com.thomasnolan;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EducateYourselfTest {
+
+    private final InputStream standardIn = System.in;
+    private final PrintStream standardOut = System.out;
+
+    @AfterEach
+    public void tearDown() {
+        System.setIn(standardIn);
+        System.setOut(standardOut);
+    }
+
+
+    /**
+     * Tests if ConnectToNetworkSquare.run() works correctly
+     */
+    @Test
+    public void educateYourselfTest() {
+        String data = "4\nyes";
+        GameIOHandler io = new GameIOHandler(
+                new ByteArrayInputStream(data.getBytes()),
+                new ByteArrayOutputStream()
+        );
+
+        Player player = new Player();
+        int hardware = player.hardware;
+
+        EducateYourselfSquare educateYourselfSquare = new EducateYourselfSquare(io);
+        boolean result = educateYourselfSquare.run(player);
+
+        assertEquals(hardware, player.hardware);
+        assertTrue(result);
+
+    }
+}
